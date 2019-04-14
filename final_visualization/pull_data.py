cf0f2c2db0916ec4b0e8dac98bfee872f8a7e101
Kevin Palm <kevinpalm24@gmail.com>
2019-04-14 11:12:10 -0700

    dropped chats from the final visualization which were too short to have a word in the model vocab

--- final_visualization/pull_data.py
+++ final_visualization/pull_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+from gensim.utils import SaveLoad
 
 def main():
 
@@ -23,15 +24,33 @@ def main():
 		, from_userid
 		, topic_id as Chat_Topic
 		, Keywords
+		, lemma
 	from chats
 		join topic_labels using (chat_id)
 		join keywords using (topic_id)
+		join lemmas using (chat_id)
+
 	"""
 
+	# load up the model vocabulary so we can make sure that at least one word was included
+	vocab = SaveLoad.load('../model_development/saved_models/model_user_day_room_10.id2word')
+
 	# store to csv for ease of use in the visualization
+	df = pd.DataFrame()
 	with sqlite3.connect('../database/chat.db') as conn:
-		df = pd.read_sql(sql, conn)
-		df.to_csv('../model_development/final_dominant_topic_text_df_FULL.txt', index=False, sep='\t')
+		total_len = pd.read_sql('select count(*) from chats', conn).iloc[0,0]
+		progress = 0
+		for chunk in pd.read_sql(sql, conn, chunksize=100000):
+
+			# remove chats that were too short to have any words from the model vocabulary
+			chunk['vocab_words'] = [len(vocab.doc2bow(text)) for text in chunk['lemma'].str.split(' ').tolist()]
+			df = df.append(chunk.loc[chunk['vocab_words']>0, ['sent_at', 'from_userid', 'Chat_Topic', 'Keywords']])
+			progress += len(chunk.index)
+			print(round(progress/total_len*100, 2), '%...', end='\r')
+
+
+	df.to_csv('../model_development/final_dominant_topic_text_df_FULL.txt', index=False, sep='\t')
+	print(len(df.index), 'out of', total_len, 'chats were saved for final visualization!')
 
 
 if __name__ == '__main__':
